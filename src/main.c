a1b4721a3dc2054579e23c902c82547fbaf5bcb6
rjuez00 <rjuez00@gmail.com>
2023-04-26 23:07:08 +0200

    clean i2c_streaming code

--- src/main.c
+++ src/main.c
@@ -31,6 +31,7 @@ struct k_thread polling_thread, i2c_thread;
 struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(LED1_NODE, gpios);
 
+//SENSOR POLLING
 typedef struct sensor_tree_t {
 	bme680_manager_t bme680_device;
 	adxl345_manager_t adxl345_device;
@@ -44,17 +45,24 @@ void sensor_polling(void *p1, void *p2, void *p3)
 	gpio_pin_configure_dt(&led0, GPIO_OUTPUT_INACTIVE);
 
 	bme680_constructor(&(sensor_tree.bme680_device));
-	ultrasonic_init(&(sensor_tree.ultrasonic_device));
+	//ultrasonic_init(&(sensor_tree.ultrasonic_device));
 	while (true) {
 		k_sleep(K_MSEC(REFRESH_TIME));
 		
 		bme680_read_sensors(&(sensor_tree.bme680_device));
-		ultrasonic_duration(&(sensor_tree.ultrasonic_device));
+		//ultrasonic_duration(&(sensor_tree.ultrasonic_device));
 
 		gpio_pin_toggle_dt(&led0);
+
+		LOG_INF("T: %lf, P: %lf, H: %lf", 
+			sensor_tree.bme680_device.last_temperature,
+			sensor_tree.bme680_device.last_pressure,
+			sensor_tree.bme680_device.last_humidity);
 	}
 }
 
+
+//I2C
 uint64_t test_value = 0x8877665544332211; //this is because I don't have a real sensor yet
 
 uint8_t send_buffer = 0x00;
@@ -63,12 +71,11 @@ uint8_t* address_to_next = NULL;
 
 void i2c_start_read(uint8_t address){
 	if(address == TEST_READ){
+		//setup
 		address_to_next = (uint8_t*)&test_value;
+		left_to_send = 8;
 
-
-		send_buffer = *((uint8_t*)address_to_next);
-		address_to_next++;
-		left_to_send--;
+		i2c_load_next_streamed_value();
 	}
 }
 
@@ -83,9 +90,6 @@ void i2c_load_next_streamed_value(){
 	}
 }
 
-
-
-
 // typedef int (*i2c_target_write_requested_cb_t)(struct i2c_target_config *config);
 int our_i2c_write_requested(struct i2c_target_config *config){
 	printf("Write requested \n");
@@ -99,14 +103,12 @@ int our_i2c_read_requested(struct i2c_target_config *config, uint8_t *val){
 	i2c_load_next_streamed_value();
 	return 0;
 }
-
 // typedef int (*i2c_target_write_received_cb_t)( struct i2c_target_config *config, uint8_t val);
 int our_i2c_write_received(struct i2c_target_config *config, uint8_t addr){
 	printf("Write received for register %d\n", addr);
 	i2c_start_read(addr);
 	return 0;
 }
-
 // typedef int (*i2c_target_read_processed_cb_t)(struct i2c_target_config *config, uint8_t *val);
 int our_i2c_read_processed(struct i2c_target_config *config, uint8_t *val){
 	printf("Read processed\n");
@@ -122,10 +124,7 @@ int our_i2c_stop(struct i2c_target_config *config){
 	return 0;
 }
 
-
-
 /* Create a static initializer for a struct i2c_dt_spec */
-
 static struct i2c_target_callbacks sn_i2c_cbs = {
 		.write_requested = our_i2c_write_requested ,  // typedef int (*i2c_target_write_requested_cb_t)(struct i2c_target_config *config);
 		.read_requested = our_i2c_read_requested,   // typedef int (*i2c_target_read_requested_cb_t)(struct i2c_target_config *config, uint8_t *val);
@@ -133,7 +132,6 @@ static struct i2c_target_callbacks sn_i2c_cbs = {
 		.read_processed = our_i2c_read_processed,   // typedef int (*i2c_target_read_processed_cb_t)(struct i2c_target_config *config, uint8_t *val);
 		.stop = our_i2c_stop // typedef int (*i2c_target_stop_cb_t)(struct i2c_target_config *config);
 };
-
 
 /* Create a static initializer for a struct i2c_target_config */
 static struct i2c_target_config i2c_cfg = {
@@ -144,7 +142,6 @@ static struct i2c_target_config i2c_cfg = {
 
 const struct device* i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
 uint32_t i2c_speed_cfg = I2C_SPEED_SET(I2C_SPEED_STANDARD);
-
 
 void i2c_communication_target(void *p1, void *p2, void *p3)
 {
@@ -179,13 +176,13 @@ void main(void)
 	k_tid_t i2c_thread_tid = k_thread_create(
 		&i2c_thread, i2c_stack, K_THREAD_STACK_SIZEOF(i2c_stack), i2c_communication_target, NULL,
 		NULL, NULL, MY_PRIORITY, K_INHERIT_PERMS, K_FOREVER);
-	// k_tid_t polling_thread_tid = k_thread_create(
-	// 	&polling_thread, sensor_polling_stack, K_THREAD_STACK_SIZEOF(sensor_polling_stack),
-	// 	i2c_communication_source, NULL, NULL, NULL, MY_PRIORITY, K_INHERIT_PERMS, K_FOREVER);
+	k_tid_t polling_thread_tid = k_thread_create(
+		&polling_thread, sensor_polling_stack, K_THREAD_STACK_SIZEOF(sensor_polling_stack),
+		sensor_polling, NULL, NULL, NULL, MY_PRIORITY, K_INHERIT_PERMS, K_FOREVER);
 
 	k_thread_cpu_pin((k_tid_t) &polling_thread_tid, 0);
 	k_thread_cpu_pin((k_tid_t) &i2c_thread_tid, 1);
 
 	k_thread_start(i2c_thread_tid);
-	// k_thread_start(polling_thread_tid);
+	k_thread_start(polling_thread_tid);
 }
